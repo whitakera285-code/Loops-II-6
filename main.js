b132d50f67c8415a569eed2b375f999926e267cc
whitakera285-code <whitakera285@gmail.com>
2026-06-08 00:05:15 +0000

    Print Coordinates

--- main.js
+++ main.js
@@ -1,2 +1,12 @@
-// Write your code below
+function printCoordinates(gridSize) {
+  // Outer loop, controls X coordinate
+  for (var x = 0; x < gridSize; x++) {
+   
+    // Inner loop, controls Y coordinate
+    for (var y = 0; y < gridSize; y++) {
+      console.log("(" + x + ", " + y + ")");
+    }
+   
+  }
+}
 
